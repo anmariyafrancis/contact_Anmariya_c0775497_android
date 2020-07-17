bad5129980c9eb2d1bdb749a2349a80356a2a678
anmariyafrancis <anmariya1915@gmail.com>
2020-07-17 12:29:40 -0400

    created new java class DatabaseHelper.java

--- app/src/main/java/com/example/contact_anmariya_c0775497_android/MainActivity.java
+++ app/src/main/java/com/example/contact_anmariya_c0775497_android/MainActivity.java
@@ -3,8 +3,34 @@ package com.example.contact_anmariya_c0775497_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+    DatabaseHelper mDatabaseHelper;
+    List<PersonModel> personList;
+    List<PersonModel> searchList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
